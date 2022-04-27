2016a817672bc02ad98686a7cbe8ce06bdc53dbe
saqer23 <aljabrisaqer23@gmail.com>
2022-04-27 06:21:20 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>ninja reaction timer</h1>
+<button @click="start" :disabled="isPlaying">play</button>
+<Block v-if="isPlaying" :delay="delay" @end="endGame" />
+<Result v-if="showResult" :score="score" />
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying:false,
+      delay: null,
+      score:null,
+      showResult:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResult = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResult = true
+    }
   }
 }
 </script>
@@ -20,7 +44,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="result">
+        <p >ReactionTime : {{ score }} ms</p>
+        <p class="rank">{{ rank }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+props: ['score'],
+data(){
+    return{
+        rank:null
+    }
+},
+mounted(){
+    if(this.score < 250){
+        this.rank = 'Ninja ringers'
+    }
+    else if(this.rank <400){
+        this.rank = 'Rapid reflixes'
+    }
+    else{
+        this.rank = 'snail pace ...'
+    }
+}
+}
+</script>
+
+<style>
+    .result{
+        color: white;
+        background: rgb(5, 230, 181);
+        border: #444 1px;
+        border-radius: 10px;
+        width: 300px;
+        text-align: center;
+        padding: 50px 0;
+        margin: 40px auto;
+    }
+    .rank{
+        font-size: 1.4em;
+        color: white;
+        font-weight: bold;
+    }
+</style>
